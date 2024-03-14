a4028534df02e32aad10b4831bff1bf306ad29fb
llydz <legaspijohnlloyd12@gmail.com>
2024-03-14 13:46:37 +0800

    lio0

--- ADMIN/register.php
+++ ADMIN/register.php
@@ -16,9 +16,22 @@
     <div class="form-container">
 
         <div class="form">
-            
+
             <H1>REGISTER</H1>
             <form action="">
+                <div class="admin">
+
+                    <div class="user"><label for="uname">ADMIN USER <span style="color: red; font-size:13px">&nbsp
+                                &nbsp*required</span></label>
+                        <input type="text" placeholder="Enter existing admin user here" name="" id=""> <br>
+                    </div>
+                    <div class="pass">
+                        <label for="uname">ADMIN PASSWORD<span style="color: red; font-size:13px">&nbsp
+                                &nbsp*required</label>
+                        <input type="password" placeholder="Enter existing admin pass here" name="" id=""> <br>
+                    </div>
+                </div>
+
                 <label for="uname">FULL NAME</label>
                 <input type="text" placeholder="Enter your name here" name="" id=""> <br>
 
@@ -28,12 +41,16 @@
                 <label for="uname">USER NAME</label>
                 <input type="text" placeholder="Enter your username here" name="" id=""> <br>
 
-                <label for="pass">PASSWORD</label>
-                <input type="password" placeholder="Enter your password here" name="" id=""><br>
-
-                <label for="pass">CONFIRM PASSWORD</label>
-                <input type="password" placeholder="Enter your password here" name="" id=""><br>
-
+                <div class="password">
+                    <div class="pass1">
+                        <label for="pass">PASSWORD</label>
+                        <input type="password" placeholder="Enter your password here" name="" id=""><br>
+                    </div>
+                    <div class="pass2">
+                        <label for="pass">CONFIRM PASSWORD</label>
+                        <input type="password" placeholder="Confirm your password" name="" id=""><br>
+                    </div>
+                </div>
 
             </form>
             <div class="baba">
